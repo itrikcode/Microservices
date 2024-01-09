052555093a7b2c2070afaafd9e83250e1833c405
itrikcode <kumarviveklohaman@gmail.com>
2024-01-09 10:09:57 +0530

    add trace.

--- trip-consumer-app/src/main/java/com/itsp/TripConsumerAppApplication.java
+++ trip-consumer-app/src/main/java/com/itsp/TripConsumerAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TripConsumerAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TripConsumerAppApplication.main()");
 		SpringApplication.run(TripConsumerAppApplication.class, args);
 	}
 
