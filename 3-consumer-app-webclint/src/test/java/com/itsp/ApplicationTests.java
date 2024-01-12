3d2d522698107129888ee301dcbc1314eb423f25
itrikcode <kumarviveklohaman@gmail.com>
2024-01-12 17:12:10 +0530

    add mege

---
+++ 3-consumer-app-webclint/src/test/java/com/itsp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
